5a2136ef085e58f9c21e42cb24f0c62bff891d53
Eric Nguyen <erictn3@uci.edu>
2021-05-01 23:59:44 -0700

    info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Eric Taylor Nguyen',
+  headline: 'Understanding the Journey',
+  description: 'I am a Full Stack Web Developer and have been studying for nearly a decade',
+  buttonLabel: 'More Info',
+  imgStart: false,
+  img: require('../../images/svg-1.svg.png').default,
+  alt: 'test',
+  dark: true, 
+  primary: true,
+  darkText: false
+};   
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
+import { homeObjOne } from '../components/InfoSection/Data';
 
 
 const Home = () => {
@@ -17,6 +19,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />  
       <HeroSection />   
+      <InfoSection {...homeObjOne}/>
     </>
   )
 }
