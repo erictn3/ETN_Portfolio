d1422538399f576fc7bd91a57d5cbc2d8c9ed59e
Eric Nguyen <erictn3@uci.edu>
2021-06-29 12:14:44 -0700

    added portfolio links to github repo

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -23,27 +23,32 @@ const Portfolio = () => {
           <PortfolioCard>
             <PortfolioIcon src={Icon1} />
             <a href='https://infinite-lowlands-76487.herokuapp.com/login'>Pawsperity</a>
+
             {/* <PortfolioH2>Pawsperity</PortfolioH2> */}
             <PortfolioP>
               Supporting a healthy life for your pet.
             </PortfolioP>
-          </PortfolioCard>
+            <a href='https://github.com/jrdrenth/pawsperity'>Github Link</a>          
+            </PortfolioCard>
           <PortfolioCard>
             <PortfolioIcon src={Icon2} />
-            <a href="https://kjwallac.github.io/BusyParentScheduler/calendar.html">Busy Parent Scheduler</a>
+            <a href="https://kjwallac.github.io/BusyParentScheduler/calendar.html">Busy Parent Scheduler Link</a>
             {/* <PortfolioH2>Busy Parent Scheduler</PortfolioH2> */}
             <PortfolioP>
               Need help with your hectic schedule?
             </PortfolioP>
-          </PortfolioCard>
+            <a href="https://github.com/erictn3/BusyParentScheduler">Github Link</a>          
+            </PortfolioCard>
           <PortfolioCard>
             <PortfolioIcon src={Icon3} />
             <a href="http://solarstein.io">Solar Stein</a>
+
             {/* <PortfolioH2>Project 3</PortfolioH2> */}
             <PortfolioP>
               Organizing and tracking your job search journey.
             </PortfolioP>
-          </PortfolioCard>
+            <a href="https://github.com/erictn3/Solar_Stein">Github Link</a>          
+            </PortfolioCard>
         </PortfolioWrapper>
     </PortfolioContainer>
   );
